ec7eb404ad9763733fff4f1d935ce846e3952736
jonghun <whdgns4288@naver.com>
2026-08-12 21:06:35 +0900

    요구 사항 정리 및 최소한의 세팅
    인프라에 필요한 도커 컴포즈 , 테스트 컨테이너 , 빌드 , 요구사항 md

--- src/test/java/com/pickbit/jobpostingservice/TestSupport.java
+++ src/test/java/com/pickbit/jobpostingservice/TestSupport.java
@@ -1,4 +1,28 @@
 package com.pickbit.jobpostingservice;
 
-public class TestSuport {
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+public abstract class TestSupport {
+
+    static final MySQLContainer<?> MYSQL = new MySQLContainer<>("mysql:8.4.5")
+            .withDatabaseName("pickbit_test_db")
+            .withUsername("test_user")
+            .withPassword("test_pass");
+
+    static {
+        MYSQL.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MYSQL::getJdbcUrl);
+        registry.add("spring.datasource.username", MYSQL::getUsername);
+        registry.add("spring.datasource.password", MYSQL::getPassword);
+    }
 }
